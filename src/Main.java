a9d9945ce56e42ff0c8780df22258c4aa2c4a73e
ramakrishnae1 <31855653+ramakrishnae1@users.noreply.github.com>
2017-09-13 13:29:22 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		StudentGroup s1=new StudentGroup();
+		s1.setStudents(3);
+		
+		StudentGroup s2=new StudentGroup();
+		s2.setStudent(1);
+		System.out.println(s1.getStudents);
+		System.out.println(s1.getStudent);
+
+
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
